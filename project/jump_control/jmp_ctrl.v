77cf5ca303efba95134225f16016da6596768580
juan-sb <sbruzzi.juan@outlook.com>
2024-06-12 01:44:20 -0300

    Fix

--- project/jump_control/jmp_ctrl.v
+++ project/jump_control/jmp_ctrl.v
@@ -8,29 +8,41 @@ module jmp_ctrl(
 	input alu_z,
 	input alu_n,
 	
+	input wire [2:0] alu_funct3,
+	input wire [16:0] alu_flags,
+	input wire [31:0] alu_pc,
+
+	
 	input clk,
 	input ena,
 	input x,
 	input nreset,
 	
 	output wire pc_wr,
-	output wire [31:0] pc_out,
+	output reg [31:0] pc_out,
 	output wire branch_taken,
 	output wire was_predicted_taken
 );
 
-wire is_jalr = flags[10];
-wire is_branch = flags[12];
-assign was_predicted_taken = flags[16];
+wire is_jalr = ena & flags[9];
+wire is_branch = ena & alu_flags[11];
+assign was_predicted_taken = ena & alu_flags[16];
 
-wire branch_beq_bne = (funct3[2:1] == 2'b00) && ((!funct3[0]) == alu_z); // 0: beq, 1: bne
-wire branch_other = ((funct3[2:1] == 2'b10) || (funct3[2:1] == 2'b11)) && (funct3[0] ^ alu_n); // 0: blt, branch si n=1, 1: bge, branch si n=0, unsigned o signed
+wire branch_beq_bne = (alu_funct3[2:1] == 2'b00) && ((!alu_funct3[0]) == alu_z); // 0: beq, 1: bne
+wire branch_other = ((alu_funct3[2:1] == 2'b10) || (alu_funct3[2:1] == 2'b11)) && (alu_funct3[0] ^ alu_n); // 0: blt, branch si n=1, 1: bge, branch si n=0, unsigned o signed
 assign branch_taken = is_branch && (branch_other || branch_beq_bne);
 wire [31:0] rs1plusimm = (rs1 + imm) & 32'hFFFFFFFE;
 wire [31:0] pcplusimm = pc + imm;
-wire [31:0] pcplus4 = pc + 4;
+wire [31:0] pcplus4 = alu_pc + 4;
 assign pc_wr = ((~nreset) || (~ena)) ? 1'b0 : (is_jalr | (branch_taken ^ was_predicted_taken));
-assign pc_out = is_jalr ? rs1plusimm : (branch_taken && !was_predicted_taken ? pcplusimm : pcplus4);
+// assign pc_out = is_branch ? (branch_taken && !was_predicted_taken ? pcplusimm : pcplus4) :  rs1plusimm;
 
+always @(branch_taken, was_predicted_taken, is_branch, is_jalr) begin
+	if(is_branch && (branch_taken ^ was_predicted_taken)) begin
+		if(branch_taken && !was_predicted_taken) pc_out <= pcplusimm;
+		else pc_out <= pcplus4;
+	end
+	else pc_out <= rs1plusimm;
+end
 
 endmodule
--- project/pipeline/pipeline_halt_control.v
+++ project/pipeline/pipeline_halt_control.v
@@ -11,15 +11,21 @@ module pipeline_halt_control(
 	input wire [4:0] alu_rd,
 	input wire [16:0] post_alu_flags,
 	input wire [4:0] post_alu_rd,
+	input wire branch_taken,
+	input wire clk,
 	output reg fetch_en,
 	output reg decoded_latch_en,
+	output reg decoded_x,
 	output reg reg_access_latch_en,
+	output reg reg_access_x,
 	output reg alu_latch_en,
+	output reg alu_x,
 	output wire jmpctrl_en
 );
 
+wire was_predicted_taken = alu_flags[16];
 wire regacc_is_jalr = reg_access_flags[9];
-wire regacc_is_branch = reg_access_flags[11];
+wire alu_is_branch = alu_flags[11];
 
 // -------- Decoder has instruction that needs a register that will be written
 wire decoded_needs_regaccess_write = reg_access_flags[0] && (reg_access_rd != 5'b0) && ((decoded_rs1 === reg_access_rd) || (decoded_rs2 === reg_access_rd));
@@ -40,31 +46,44 @@ assign regaccess_blocked = regaccess_needs_alu_write || regaccess_needs_postalu_
 // ---------
 
 // -------- JALR jmp ctrl && branch
-assign jmpctrl_en = regacc_is_jalr || regacc_is_branch;
+wire branch_misspredict = was_predicted_taken ^ branch_taken;
+assign jmpctrl_en = regacc_is_jalr | alu_is_branch;
 //
 
 
-always @(decoded_blocked or regaccess_blocked) begin
+always @(decoded_blocked, regaccess_blocked, branch_misspredict) begin
 	fetch_en <= 1;
 	decoded_latch_en <= 1;
 	reg_access_latch_en <= 1;
 	alu_latch_en <= 1;
+	decoded_x <= 0;
+	reg_access_x <= 0;
+	alu_x <= 0;
 	
 	if(decoded_blocked) begin
 		decoded_latch_en <= 0;
 		fetch_en <= 0;
-	end else 
+		// reg_access_x <= 1;
+	end else begin
 		decoded_latch_en <= 1;
-	
 		
-	if(regaccess_blocked) begin
+		if(regaccess_blocked) begin
+			reg_access_latch_en <= 0;
+			decoded_latch_en <= 0;
+			fetch_en <= 0;
+			// reg_access_x <= 0;
+			// alu_x <= 0;
+		end else
+			reg_access_latch_en  <= 1;
+	end
+		/*
+	if(branch_misspredict) begin
 		reg_access_latch_en <= 0;
-		decoded_latch_en <= 0;
-		fetch_en <= 0;
-	end else
-		reg_access_latch_en  <= 1;
-		
+		alu_latch_en <= 0;
+	end
+	*/
 end
+
 
 
 endmodule
